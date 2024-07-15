60a87272351a41f39d43422c87e04d3569ed05cb
Logeswaran A <lokeshwaran.a82@gmail.com>
2024-07-15 15:34:11 +0400

    updated

--- basics/median.py
+++ basics/median.py
@@ -12,4 +12,7 @@ def find_median(listvalue):
 numbers = [5,1,3,2,1,5,6]
 print(find_median(numbers))
 
+numbers = [3,5,1,4]
+print(find_median(numbers))
+
 # // operator is integer division operator, which divides and will round the results to nearest integer value
